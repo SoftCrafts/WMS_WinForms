81cbe65ab2411a51b88f4a62d393d0302318c769
firkosa <firkosa@abv.bg>
2014-08-16 21:48:18 +0300

    dadad

---
+++ WMS/WarehouseForms/ModelCard.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WMS.WarehouseForms
+{
+    partial class ModelCard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radTextBox1 = new Telerik.WinControls.UI.RadTextBox();
+            this.radTextBox2 = new Telerik.WinControls.UI.RadTextBox();
+            this.radTextBox3 = new Telerik.WinControls.UI.RadTextBox();
+            this.lbl_ItemName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.radButton2 = new Telerik.WinControls.UI.RadButton();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radTextBox1
+            // 
+            this.radTextBox1.Location = new System.Drawing.Point(116, 114);
+            this.radTextBox1.Name = "radTextBox1";
+            this.radTextBox1.Size = new System.Drawing.Size(148, 20);
+            this.radTextBox1.TabIndex = 0;
+            // 
+            // radTextBox2
+            // 
+            this.radTextBox2.Location = new System.Drawing.Point(116, 140);
+            this.radTextBox2.Name = "radTextBox2";
+            this.radTextBox2.Size = new System.Drawing.Size(148, 20);
+            this.radTextBox2.TabIndex = 1;
+            // 
+            // radTextBox3
+            // 
+            this.radTextBox3.Location = new System.Drawing.Point(116, 166);
+            this.radTextBox3.Name = "radTextBox3";
+            this.radTextBox3.Size = new System.Drawing.Size(148, 20);
+            this.radTextBox3.TabIndex = 2;
+            // 
+            // lbl_ItemName
+            // 
+            this.lbl_ItemName.AutoSize = true;
+            this.lbl_ItemName.Location = new System.Drawing.Point(75, 116);
+            this.lbl_ItemName.Name = "lbl_ItemName";
+            this.lbl_ItemName.Size = new System.Drawing.Size(35, 13);
+            this.lbl_ItemName.TabIndex = 3;
+            this.lbl_ItemName.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // radButton1
+            // 
+            this.radButton1.Location = new System.Drawing.Point(301, 157);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(110, 24);
+            this.radButton1.TabIndex = 6;
+            this.radButton1.Text = "Запиши";
+            // 
+            // radButton2
+            // 
+            this.radButton2.Location = new System.Drawing.Point(12, 12);
+            this.radButton2.Name = "radButton2";
+            this.radButton2.Size = new System.Drawing.Size(110, 24);
+            this.radButton2.TabIndex = 7;
+            this.radButton2.Text = "Назад";
+            this.radButton2.Click += new System.EventHandler(this.radButton2_Click);
+            // 
+            // ModelCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 357);
+            this.ControlBox = false;
+            this.Controls.Add(this.radButton2);
+            this.Controls.Add(this.radButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_ItemName);
+            this.Controls.Add(this.radTextBox3);
+            this.Controls.Add(this.radTextBox2);
+            this.Controls.Add(this.radTextBox1);
+            this.Name = "ModelCard";
+            this.Text = "ModelCard";
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadTextBox radTextBox1;
+        private Telerik.WinControls.UI.RadTextBox radTextBox2;
+        private Telerik.WinControls.UI.RadTextBox radTextBox3;
+        private System.Windows.Forms.Label lbl_ItemName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadButton radButton2;
+    }
+}
---
+++ WMS/WarehouseForms/Models.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WMS.WarehouseForms
+{
+    public partial class Models : Form
+    {
+        public Models()
+        {
+            InitializeComponent();
+        }
+
+        private void Models_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dataSet1.Model' table. You can move, or remove it, as needed.
+      
+
+        }
+
+        private void radButton1_Click(object sender, EventArgs e)
+        {
+            ModelCard mc = new ModelCard();
+            mc.TopLevel = false;
+            mc.Dock = DockStyle.Fill;
+            mc.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            mc.Parent = this;
+            mc.WindowState = FormWindowState.Maximized;
+            mc.Show();
+           
+        }
+    }
+}
